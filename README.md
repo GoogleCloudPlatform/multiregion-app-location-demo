46a7c7dab837cc798836c32ce8641eebb409e9bc
James Ward <james@jamesward.com>
2020-09-01 12:11:03 -0600

    try this

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 Run Locally:
 ```
-./gradlew dev
+export SEARCH_CX=YOUR_SEARCH_CX
+export SEARCH_KEY=YOUR_SEARCH_KEY
+./gradlew -t run
 ```
 
 Visit: [http://localhost:8080](http://localhost:8080)
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,13 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.jetbrains.kotlin.allopen.gradle.AllOpenExtension
 
 plugins {
     application
-    kotlin("jvm") version "1.3.21"
-    kotlin("kapt") version "1.3.21"
-    id("org.jetbrains.kotlin.plugin.allopen") version "1.3.21"
-    id("com.google.cloud.tools.jib") version "1.0.2"
-    id("com.github.johnrengelman.shadow") version "4.0.4"
+    kotlin("jvm") version "1.4.0"
+    kotlin("kapt") version "1.4.0"
+    kotlin("plugin.allopen") version "1.4.0"
+    id("com.google.cloud.tools.jib") version "2.5.0"
 }
 
 repositories {
@@ -20,35 +17,35 @@ repositories {
 }
 
 dependencies {
-    compile(kotlin("stdlib"))
-    compile(kotlin("reflect"))
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.7")
 
-    compile("io.micronaut:micronaut-runtime:1.1.0")
-    compile("io.micronaut:micronaut-http-client:1.1.0")
-    compile("io.micronaut:micronaut-http-server-netty:1.1.0")
-    compile("io.micronaut:micronaut-views:1.1.0")
-    compile("ch.qos.logback:logback-classic:1.2.3")
+    implementation("io.micronaut:micronaut-runtime:2.0.1")
+    implementation("io.micronaut:micronaut-http-client:2.0.1")
+    implementation("io.micronaut:micronaut-http-server-netty:2.0.1")
+    implementation("io.micronaut.views:micronaut-views-thymeleaf:2.0.0")
+
+    runtime("ch.qos.logback:logback-classic:1.2.3")
 
     runtime("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.7")
-    runtime("org.thymeleaf:thymeleaf:3.0.11.RELEASE")
 
-    kapt("io.micronaut:micronaut-inject-java:1.1.0")
-    kapt("io.micronaut:micronaut-validation:1.1.0")
+    kapt("io.micronaut:micronaut-inject-java:2.0.1")
 
-    testImplementation("org.jetbrains.kotlin:kotlin-test:1.3.21")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:1.4.0")
     testImplementation("org.spekframework.spek2:spek-dsl-jvm:2.0.1")
     testRuntimeOnly("org.spekframework.spek2:spek-runner-junit5:2.0.1")
 
-    kaptTest("io.micronaut:micronaut-inject-java:1.1.0")
+    kaptTest("io.micronaut:micronaut-inject-java:2.0.1")
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
         javaParameters = true
     }
 }
@@ -68,33 +65,34 @@ tasks.withType<Test> {
     }
 }
 
-configure<AllOpenExtension> {
+allOpen {
     annotation("io.micronaut.aop.Around")
 }
 
-tasks.all {
-    when(this) {
-        is JavaForkOptions -> {
-            jvmArgs("-noverify")
-            jvmArgs("-XX:TieredStopAtLevel=1")
-        }
+kapt {
+    arguments {
+        arg("micronaut.processing.incremental", true)
+        arg("micronaut.processing.annotations", "whereami.*")
+        arg("micronaut.processing.group", "whereami")
+        arg("micronaut.processing.module", "whereami")
+    }
+}
+
+tasks.withType<JavaExec> {
+    jvmArgs = listOf("-XX:TieredStopAtLevel=1", "-Dcom.sun.management.jmxremote")
+
+    if (gradle.startParameter.isContinuous) {
+        systemProperties = mapOf(
+                "micronaut.io.watch.restart" to "true",
+                "micronaut.io.watch.enabled" to "true",
+                "micronaut.io.watch.paths" to "src/main"
+        )
     }
 }
 
 jib {
-    val projectId: String? by project
-
-    to.image = "gcr.io/$projectId/where-am-i"
     container {
         mainClass = application.mainClassName
         ports = listOf("8080")
     }
-}
-
-tasks.withType<ShadowJar> {
-    mergeServiceFiles()
-}
-
-tasks.create("stage") {
-    dependsOn("shadowJar")
 }
